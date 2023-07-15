e39216d5fbe91f1e4ee8d27d941191077a6e38d8
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-07-15 16:31:37 -0700

    Chunk Writing
    Don't know if this works yet, not sure how to test it for comparison. I think it doesn't work 😂

--- src/chunk.ts
+++ src/chunk.ts
@@ -1,13 +1,21 @@
-import { read as readNBT } from "nbtify";
+import { read as readNBT, write as writeNBT } from "nbtify";
 
-import type { NBTData } from "nbtify";
+import type { FormatOptions, NBTData } from "nbtify";
 
 export const HEADER_LENGTH = 5;
+export const SCHEME_LENGTH = 1;
 
 export type Compression = "gzip" | "deflate" | null;
 export type CompressionScheme = 1 | 2 | 3;
 
-export type Chunk = NBTData;
+export interface Format extends FormatOptions {
+  name: "";
+  endian: "big";
+  compression: Compression;
+  bedrockLevel: null;
+}
+
+export type Chunk = NBTData<any,Format>;
 
 export interface Header {
   byteLength: number;
@@ -22,7 +30,7 @@ export async function readChunk(chunk: Uint8Array): Promise<Chunk> {
 
 export function readHeader(chunk: Uint8Array): Header {
   const view = new DataView(chunk.buffer,chunk.byteOffset,chunk.byteLength);
-  const byteLength = view.getUint32(0) - 1;
+  const byteLength = view.getUint32(0) - SCHEME_LENGTH;
   const scheme = view.getUint8(4) as CompressionScheme;
   const compression = readCompression(scheme);
   return { byteLength, compression };
@@ -34,5 +42,32 @@ export function readCompression(scheme: CompressionScheme): Compression {
     case 2: return "deflate";
     case 3: return null;
     default: throw new TypeError(`Encountered unsupported compression scheme '${scheme}', must be a valid compression type`);
+  }
+}
+
+export async function writeChunk(chunk: Chunk): Promise<Uint8Array> {
+  let data = await writeNBT(chunk);
+  const { byteLength } = data;
+  const { compression } = chunk;
+  data = writeHeader(data,{ byteLength, compression });
+  return data;
+}
+
+export function writeHeader(chunk: Uint8Array, { byteLength, compression }: Header): Uint8Array {
+  const view = new DataView(chunk.buffer,chunk.byteOffset,chunk.byteLength);
+  const scheme = writeCompression(compression);
+  const data = new Uint8Array(HEADER_LENGTH + byteLength);
+  view.setUint32(0,byteLength + SCHEME_LENGTH);
+  view.setUint8(4,scheme);
+  data.set(chunk,HEADER_LENGTH);
+  return data;
+}
+
+export function writeCompression(compression: Compression): CompressionScheme {
+  switch (compression){
+    case "gzip": return 1;
+    case "deflate": return 2;
+    case null: return 3;
+    default: throw new TypeError(`Encountered unsupported compression type '${compression}', must be a valid compression scheme`);
   }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,22 +1,15 @@
 import { readFile } from "node:fs/promises";
 import { readRegion } from "../src/index.js";
 
-import type { Chunk } from "../src/index.js";
+const REGION = new URL("./test-flat!/region/r.0.0.mca",import.meta.url);
 
-const AXES: [number,number][] = [
-  [-1,-1],
-  [-1,-2],
-  [-1,0],
-  [0,-1],
-  [0,-2],
-  [0,0]
-];
+const data = await readFile(REGION);
+const region = await readRegion(data);
 
-const REGIONS = AXES.map(([x,y]) => new URL(`./test-flat!/region/r.${x}.${y}.mca`,import.meta.url));
+for (const chunk of region){
+  if (chunk === null) continue;
+  const { xPos, yPos, zPos } = chunk.data;
+  console.log({ xPos, yPos, zPos });
+}
 
-const datas = await Promise.all(REGIONS.map(axis => readFile(axis)));
-// console.log(data.subarray(0,800).join(" "));
-for (const data of datas) console.log(data);
-
-const regions = await Promise.all(datas.map(data => readRegion(data)));
-console.log(regions);
+console.log(region.length);
