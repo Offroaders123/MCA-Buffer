cd51ebaa44060d37e6ac1b199e021913c6f59270
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-07-15 16:52:41 -0700

    Chunk Buffer Sizes + 2D Array Demo
    Wanted to figure out more about how chunk data is written to the buffer (region too), and whether or not it's a fixed size.
    
    I think I still need to solve a disparity between the chunks and their timestamp/location values. Not sure if that's with respective Region and Chunk objects/classes, but I think I have to tie them together somehow.
    
    Also, just looked into how to do 2d arrays, and it actually makes a bit more sense to me now! It's not like how you'd think it'd work (at least, not for me). I thought you'd have to have a new array primitive type, but when you think about it, you can just nest arrays inside each other instead, and access the different axes inside those actual arrays. So, picture this structure (won't make sense, if you're not smert like me XD)
    
    ```
    /** what is this naming */
    const twoDArray = [
      [
        33, 25, 74
      ],
      [
        45
      ]
    ] as const;
    // note that this grid is essentially just 'number[][]'
    
    const myValueAtPositionXY: 33 = twoDArray[0][0];
    ```
    
    So, you don't as much think of it like how the objects look, but by how you index them. Maybe instead, I've been thinking of coordinates differently than to how they actually work too, and maybe that's my holdup. I think there's merits to understanding both sides of it.
    
    It feels like a very similar thing about 'perspective'. Just like when I learned while making Flatlands, that you have to move the world around your character, to make it look like the character is moving.
    
    I think this is probably the way I'll go for Region representations, since it makes it easier to get chunks at specific coordinates and move them around to different ones too. Wow this is really cool! I'm curious if this is how the game itself represents these 'primitives'. I'm not sure what the other ways are to manage it other than like this, unless you manually set up some kind of crazy API to mimic that kind of indexing behavior (what I was likely going to do, having not discovered this, lol).
    
    https://www.freecodecamp.org/news/javascript-2d-arrays/
    https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript

--- test/index.ts
+++ test/index.ts
@@ -1,15 +1,23 @@
 import { readFile } from "node:fs/promises";
-import { readRegion } from "../src/index.js";
+import { readRegion, readLocations, readChunkLocation, writeChunk } from "../src/index.js";
 
 const REGION = new URL("./test-flat!/region/r.0.0.mca",import.meta.url);
 
 const data = await readFile(REGION);
 const region = await readRegion(data);
 
-for (const chunk of region){
-  if (chunk === null) continue;
-  const { xPos, yPos, zPos } = chunk.data;
-  console.log({ xPos, yPos, zPos });
-}
+const regionData = readLocations(data)
+  .map(location => readChunkLocation(data,location))
+  .filter((chunkData): chunkData is Uint8Array => chunkData !== null);
 
-console.log(region.length);
+for (const chunkData of regionData) console.log(chunkData);
+
+console.log(new Set(regionData.map(chunkData => chunkData.byteLength)));
+
+// for (const chunk of region){
+//   if (chunk === null) continue;
+//   const { xPos, yPos, zPos } = chunk.data;
+//   console.log({ xPos, yPos, zPos });
+// }
+
+// console.log(region.length);
