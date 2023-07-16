847a03f43dbfe0ad7579ef3377c695db01201919
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-07-15 17:51:59 -0700

    3D Object Indexing
    Sick, and Haken 1985 is one of my favorite songs now.

--- src/region.ts
+++ src/region.ts
@@ -4,19 +4,19 @@ import { readChunk } from "./chunk.js";
 import type { Location } from "./location.js";
 import type { Chunk } from "./chunk.js";
 
-export type Region = Chunk[][][];
+export type Region = Record<number,Record<number,Record<number,Chunk>>>;
 
 export async function readRegion(region: Uint8Array): Promise<Region> {
   const locations = readLocations(region);
-  const chunks: Region = [];
+  const chunks: Region = {};
 
   await Promise.all(locations.map(async location => {
     const data = readChunkLocation(region,location);
     if (data === null) return;
     const chunk = await readChunk(data);
     const { x, y, z } = chunk.getPos();
-    if (chunks[x] === undefined) chunks[x] = [];
-    if (chunks[x][y] === undefined) chunks[x][y] = [];
+    if (chunks[x] === undefined) chunks[x] = {};
+    if (chunks[x][y] === undefined) chunks[x][y] = {};
     chunks[x][y][z] = chunk;
   }));
 
