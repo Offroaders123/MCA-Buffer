b11e0d275760157d0f5a7f55dda97403b1b22e04
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-10-16 14:29:35 -0700

    Metadata Generators
    Also from MCRegionJS, I do afterall like Generators more for reading the metadata entries like this, rather then returning plain arrays of the values.

--- src/region.ts
+++ src/region.ts
@@ -24,7 +24,7 @@ export class Region {
 }
 
 export async function readRegion(region: Uint8Array): Promise<Region> {
-  const locations = readLocations(region);
+  const locations: Location[] = [...readLocations(region)];
   const chunks = new Region();
 
   await Promise.all(locations.map(async location => {
@@ -46,17 +46,15 @@ export interface Location {
   byteLength: number;
 }
 
-export function readLocations(region: Uint8Array): Location[] {
-  const view = new DataView(region.buffer,region.byteOffset,region.byteLength);
-  const locations: Location[] = [];
+export function* readLocations(region: Uint8Array): Generator<Location,void,void> {
+  const view = new DataView(region.buffer,region.byteOffset,LOCATIONS_LENGTH);
 
   for (let i = LOCATIONS_OFFSET; i < LOCATIONS_OFFSET + LOCATIONS_LENGTH; i += LOCATION_LENGTH){
     const byteOffset = (view.getUint32(i) >> 8) * LOCATIONS_LENGTH;
     const byteLength = view.getUint8(i + 3) * LOCATIONS_LENGTH;
-    locations.push({ byteOffset, byteLength });
-  }
 
-  return locations;
+    yield { byteOffset, byteLength };
+  }
 }
 
 export const TIMESTAMP_LENGTH = 4;
@@ -65,16 +63,12 @@ export const TIMESTAMPS_OFFSET = LOCATIONS_LENGTH;
 
 export type Timestamp = number;
 
-export function readTimestamps(region: Uint8Array): Timestamp[] {
-  const view = new DataView(region.buffer,region.byteOffset,region.byteLength);
-  const timestamps: Timestamp[] = [];
+export function* readTimestamps(region: Uint8Array): Generator<Timestamp,void,void> {
+  const view = new DataView(region.buffer,region.byteOffset,TIMESTAMPS_LENGTH);
 
   for (let i = TIMESTAMPS_OFFSET; i < TIMESTAMPS_OFFSET + TIMESTAMPS_LENGTH; i += TIMESTAMP_LENGTH){
-    const timestamp = view.getUint32(i);
-    timestamps.push(timestamp);
+    yield view.getUint32(i);
   }
-
-  return timestamps;
 }
 
 export function readChunkLocation(region: Uint8Array, { byteOffset, byteLength }: Location): Uint8Array | null {
