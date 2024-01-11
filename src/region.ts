e071554f75a02c1b6381998224005c1ecb422d12
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-01-11 03:08:23 -0800

    Region Debugging
    Looking into trying to figure out how entries are stored/ordered in the Region file, since it doesn't use the coordinates themselves, at least in terms of where the Chunk for those coordinates is saved within the Region file. I think it uses a stack-based thing, where the most recently edited Chunk is tacked on to the end, and the missing gap is simply just copied over cleanly, the rest of the Region doesn't have to be rearead or serialized again.
    
    https://minecraft.wiki/w/Region_file_format
    
    Wow, these came in handly very well, for some reason this week has been a bit tougher lately, it's crazy how many of these are relatable.
    
    https://www.youtube.com/watch?v=dlpezhtrATQ
    https://www.youtube.com/watch?v=yDdIijyAn6k
    https://www.youtube.com/watch?v=Tl20Ke2Y58g
    
    Dark Empath is something I've never heard of before, nor thought of being possible! Very intriguing.
    
    I want to make this Region parser return a `JavaEntry | LegacyConsoleEntry`, which means the entry header parsing is left for after the chunking step. I think this is a more streamlined approach, and it's better at following the patterns like that which I have been using in my own projects.
    
    If any of this commit message made no sense, it's because I'm almost falling asleep while writing this, getting real tired at night for once, yay!!!
    
    I still have more to do, but this is a good pausing point for the night, and it's a good middleground to commit/track before I make the new official changes to it.

--- src/region.ts
+++ src/region.ts
@@ -1,63 +1,27 @@
-import type { Compression } from "nbtify";
+import { read } from "nbtify";
+
+export const REGION_LENGTH = 1024;
+
+const format = { rootName: true, endian: "big", compression: "deflate", bedrockLevel: false } as const;
 
 export interface Region extends ReadonlyArray<Entry | null> {
   [index: number]: Entry | null;
 }
 
-export function readRegion(region: Uint8Array): Region {
-  const entries = [...readEntries(region)];
-  // entries.sort((a,b) => (a?.byteOffset ?? 0) - (b?.byteOffset ?? 0));
+export async function readRegion(region: Uint8Array): Promise<Region> {
+  const entries: Region = Object.seal(Array<Region[number]>(REGION_LENGTH).fill(null));
+  const view = new DataView(region.buffer,region.byteOffset,region.byteLength);
 
-  const editIndicies = [...entries]
-    .sort((previous,next) => (previous?.data.byteOffset ?? 0) - (next?.data.byteOffset ?? 0));
-
-  for (const [i,entry] of editIndicies.entries()){
-    if (entry === null) continue;
-    entry.index = i;
+  for (let i = LOCATIONS_OFFSET; i < LOCATIONS_OFFSET + LOCATIONS_LENGTH; i += LOCATION_LENGTH){
+    const byteOffset = (view.getUint32(i) >> 8) * ENTRY_LENGTH;
+    const byteLength = view.getUint8(i + 3) * ENTRY_LENGTH;
+    const timestamp = view.getUint32(i + TIMESTAMPS_OFFSET);
+    const data: Uint8Array | null = byteLength !== 0 ? (await read(region.subarray(byteOffset + 5,byteOffset + byteLength),format)).data : null;
+    if (data) var { xPos, yPos, zPos } = data;
+    entries[i / LOCATION_LENGTH] = { data: data !== null ? { xPos, yPos, zPos } : data, byteOffset, timestamp };
   }
 
-  return Object.seal(entries);
-}
-
-export function writeRegion(region: Region): Uint8Array {
-  const regionLength: number = region
-    .map(entry => Math.ceil((entry?.data.byteLength ?? 0) / ENTRY_LENGTH) * ENTRY_LENGTH)
-    .reduce((previous,current) => previous + current,LOCATIONS_OFFSET + LOCATIONS_LENGTH + TIMESTAMPS_LENGTH);
-
-  const data = new Uint8Array(regionLength);
-  const view = new DataView(data.buffer);
-
-  let writePointer = LOCATIONS_OFFSET + LOCATIONS_LENGTH + TIMESTAMPS_LENGTH;
-
-  const offsets = region
-    .map((entry,i) => {
-      if (entry === null) return { coordinate: i, index: 0 };
-      return { ...entry, coordinate: i };
-    })
-    .sort((previous,next) => (previous.index ?? 0) - (next.index ?? 0))
-    .map(entry => {
-      if (!("data" in entry)) return { ...entry, byteOffset: 0, byteLength: 0 };
-      const byteLength: number = Math.ceil(entry.data.byteLength / ENTRY_LENGTH);
-      const byteOffset: number = writePointer;
-      writePointer += byteLength;
-      return { ...entry, byteOffset, byteLength };
-    })
-    .sort((previous,next) => previous.coordinate - next.coordinate);
-  console.log(offsets);
-
-  for (const { coordinate: i, byteOffset, byteLength } of offsets){
-    // console.log(i,entry?.index);
-    // const byteLength: number = Math.ceil((entry?.data.byteLength ?? 0) / ENTRY_LENGTH);
-    // const byteOffset: number = offsets[i];
-    view.setUint32(i * LOCATION_LENGTH,byteOffset);
-    view.setUint8(i * LOCATION_LENGTH + 3,byteLength);
-
-    if (i === 17) console.log(byteOffset,byteLength * ENTRY_LENGTH);
-
-    // writePointer += byteLength;
-  }
-
-  return data;
+  return entries;
 }
 
 export const LOCATIONS_OFFSET = 0;
@@ -72,53 +36,7 @@ export const ENTRY_LENGTH = 4096;
 export const ENTRY_HEADER_LENGTH = 5;
 
 export interface Entry {
-  data: Uint8Array;
+  data: Uint8Array | null;
+  byteOffset: number;
   timestamp: number;
-  compression: Compression;
-  index?: number;
 }
-
-export function* readEntries(region: Uint8Array): Generator<Entry | null, void, void> {
-  const view = new DataView(region.buffer,region.byteOffset,region.byteLength);
-
-  for (let i = LOCATIONS_OFFSET; i < LOCATIONS_OFFSET + LOCATIONS_LENGTH; i += LOCATION_LENGTH){
-    let byteOffset = (view.getUint32(i) >> 8) * ENTRY_LENGTH;
-    let byteLength = view.getUint8(i + 3) * ENTRY_LENGTH;
-    // if (i / LOCATION_LENGTH === 17) console.log(byteOffset,byteLength);
-    const timestamp = view.getUint32(i + TIMESTAMPS_OFFSET);
-
-    if (byteLength === 0){
-      yield null; continue;
-    }
-
-    byteLength = view.getUint32(byteOffset) - 1;
-    const compression = readCompressionScheme(view.getUint8(byteOffset + 4) as CompressionScheme);
-    byteOffset += ENTRY_HEADER_LENGTH;
-
-    const data = region.subarray(byteOffset,byteOffset + byteLength);
-
-    yield { data, timestamp, compression };
-  }
-}
-
-type CompressionScheme = 1 | 2 | 3;
-
-function readCompressionScheme(scheme: CompressionScheme): Compression {
-  switch (scheme){
-    case 1: return "gzip";
-    case 2: return "deflate";
-    case 3: return null;
-    default: throw new TypeError(`Encountered unsupported compression scheme '${scheme}', must be a valid compression type`);
-  }
-}
-
-/*
-function writeCompressionScheme(format: Compression): CompressionScheme {
-  switch (format){
-    case "gzip": return 1;
-    case "deflate": return 2;
-    case null: return 3;
-    default: throw new TypeError(`Encountered unsupported compression format '${format}', must be a valid compression type`);
-  }
-}
-*/
--- test/index.ts
+++ test/index.ts
@@ -1,15 +1,10 @@
 import { readFile } from "node:fs/promises";
-import { readRegion, readChunks } from "../src/index.js";
+import { readRegion } from "../src/index.js";
 
 const REGION = new URL("./r.2.1.mca",import.meta.url);
 
 const data = await readFile(REGION);
 console.log(data);
 
-const region = readRegion(data);
-// console.log(region.at(17));
-// console.log(region.length);
-
-const chunks = await readChunks(region);
-console.log(chunks.slice(17,19));
-// console.log(chunks.length);
+const region = await readRegion(data);
+region.forEach(chunk => console.log(chunk));
