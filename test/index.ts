0861efa4cc8cadeaf78dd9ed4ff14bdbb56a7889
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-01-12 23:01:42 -0800

    Symmetrical Test Checking
    I think this demo was meant to try running the read/write comparisons for Region entries on their own, outside of the overarching Region handling itself. This was from last night, to which I fell asleep before finishing or committing the progress I had so far for it. I was going to try doing a recap of what I was doing and where I was, but I don't have much time for coding tonight, so I'm going to leave that for another day, possibly tomorrow instead!

--- test/index.ts
+++ test/index.ts
@@ -1,24 +1,55 @@
 import { readFile } from "node:fs/promises";
-import { readRegion, writeRegion } from "../src/index.js";
+import { ENTRY_HEADER_LENGTH, ENTRY_LENGTH, readRegion, writeRegion } from "../src/index.js";
 
 import type { Region } from "../src/index.js";
 
 const REGION = new URL("./r.2.1.mca",import.meta.url);
 
 const data = await readFile(REGION);
-console.log(data);
+// console.log(data);
 
 // region data
 const region = readRegion(data);
 
 // nice for logging
-const prettyRegion = region.slice(15,20);
-console.log(prettyRegion);
+const prettyRegion = region;
+// console.log(prettyRegion);
 
-const redata = Buffer.from(writeRegion(region).buffer);
-console.log(redata);
+for (const entry of prettyRegion){
+  if (entry.data === null){
+    console.log(null);
+    continue;
+  }
 
-console.log("\nequality comparison:",Buffer.compare(data,redata),"== 0");
+  const start = Buffer.from(entry.data.buffer,entry.data.byteOffset,entry.byteLength);
+  const startLength = start.byteLength;
+
+  const readLength = start.readUInt32BE(0) - 1;
+  const readScheme = start.readUInt8(4);
+  const read = entry.data.subarray(ENTRY_HEADER_LENGTH,ENTRY_HEADER_LENGTH + readLength);
+
+  const writeLength = Math.ceil(readLength / ENTRY_LENGTH) * ENTRY_LENGTH;
+  const write = Buffer.alloc(writeLength);
+  write.writeUInt32BE(readLength + 1,0);
+  write.writeUInt8(readScheme,4);
+  write.set(read,ENTRY_HEADER_LENGTH);
+
+  const compare = Buffer.compare(start,write);
+
+  if (compare === 0) continue;
+  console.log([
+    { startLength },
+    { readLength, readScheme },
+    { writeLength },
+    ["compare:",compare]
+  ]);
+  console.log(start,write);
+}
+
+// const redata = Buffer.from(writeRegion(region).buffer);
+// // console.log(redata);
+
+// console.log("\nequality comparison:",Buffer.compare(data,redata),"== 0");
 
 // function prettyPrint(region: Region): Omit<Region[number], "data">[] {
 //   return region.map(({ index, timestamp, byteOffset }) => ({ index, timestamp, byteOffset }));
