a5275ff236fb58b89538203da61b4f7f912f1957
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-02-10 15:13:09 -0800

    Chunk Subclass Removal
    Realized recently that I can also use a custom interface type on top of `NBTData`, rather than extending it as a class. That actually seems like a nice way to go, since it doesn't mean you have to use any constructors or `instanceof` checks. It simply will only allow you to get and set properties on that data object, if it's the type you intend it to be. So say like for how I am using `Object.assign()` in the code here.
    
    My changes in `region.ts` broke how things are handled here, and it's still a bit weird. I have to make the region parsing generic to work with any platform or region version, as I don't want to maintain separate ones for each edition.

--- src/chunk.ts
+++ src/chunk.ts
@@ -2,12 +2,11 @@ import { read, write, NBTData } from "nbtify";
 
 import type { Format } from "nbtify";
 import type { Chunk as ChunkData } from "../Region-Types/src/java/index.js";
-import type { Region, Entry } from "./region.js";
+import type { Region, JavaEntry } from "./region.js";
 
-export class Chunk extends NBTData<ChunkData> {
-  constructor(data: NBTData<ChunkData>, public timestamp: number, public index?: number) {
-    super(data);
-  }
+export interface Chunk extends NBTData<ChunkData> {
+  timestamp: number;
+  index?: number;
 }
 
 export const CHUNK_NBT_FORMAT = {
@@ -21,18 +20,18 @@ export async function readChunks(region: Region): Promise<(Chunk | null)[]> {
   return Promise.all(region.map(readEntry));
 }
 
-export async function readEntry(entry: Entry | null): Promise<Chunk | null> {
-  if (entry === null) return null;
+export async function readEntry(entry: JavaEntry): Promise<Chunk | null> {
+  if (entry.data === null) return null;
   const { data, timestamp, compression, index } = entry;
   const nbt: NBTData<ChunkData> = await read(data,{ ...CHUNK_NBT_FORMAT, compression });
-  return new Chunk(nbt,timestamp,index);
+  return Object.assign(nbt,{ timestamp, index });
 }
 
 export async function writeChunks(chunks: (Chunk | null)[]): Promise<Region> {
   return Promise.all(chunks.map(writeEntry));
 }
 
-export async function writeEntry(chunk: Chunk | null): Promise<Entry | null> {
+export async function writeEntry(chunk: Chunk | null): Promise<JavaEntry | null> {
   if (chunk === null) return null;
   const { data: nbt, timestamp, compression, index } = chunk;
   const data: Uint8Array = await write(nbt,{ ...CHUNK_NBT_FORMAT, compression });
