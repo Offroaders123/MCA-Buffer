872e24265006e84e541ae2bb100ecac175c749a6
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-05-13 02:36:21 -0700

    Region Coordinate Calculations
    I also used Chunk Base as a way to conceptualize if I'm doing the math right, as well as the logic for this being derived from functions in the mc-anvil project.
    
    https://github.com/hpratt/mc-anvil/blob/7dfc8e46e6c2ee4597eab4f93a91fda1b6ff97f7/src/anvil/chunk/chunk.ts#L11
    https://stackoverflow.com/questions/1730961/convert-a-2d-array-index-into-a-1d-index
    https://www.reddit.com/r/csharp/comments/1b5wglp/how_to_convert_a_2d_index_into_a_1d_index/

--- src/region.ts
+++ src/region.ts
@@ -1,5 +1,3 @@
-import type { Compression } from "nbtify";
-
 export const LOCATIONS_OFFSET = 0;
 export const LOCATIONS_LENGTH = 4096;
 export const LOCATION_LENGTH = 4;
@@ -9,83 +7,43 @@ export const TIMESTAMPS_LENGTH = 4096;
 export const TIMESTAMP_LENGTH = 4;
 
 export const ENTRY_LENGTH = 4096;
-export const ENTRY_HEADER_LENGTH = 5;
 
-export const REGION_LENGTH = 1024;
-
-export interface Region<T extends EntryLike = EntryLike> extends ReadonlyArray<T> {
-  [index: number]: T;
-}
-
-export interface JavaEntry extends EntryLike {
-  compression: Compression;
-}
-
-export interface EntryLike {
-  data: Uint8Array | null;
+export interface Chunk {
+  x: number;
+  z: number;
   index: number;
-  timestamp: number;
   byteOffset: number;
   byteLength: number;
+  timestamp: number;
 }
 
-export function readRegion(region: Uint8Array): Region {
-  const entries: Region = Object.seal(Array.from<Region[number]>({ length: REGION_LENGTH }));
-  const view = new DataView(region.buffer,region.byteOffset,region.byteLength);
+export function readRegion(data: Uint8Array): Chunk[] {
+  const chunks: Chunk[] = [];
+  const view = new DataView(data.buffer, data.byteOffset, data.byteLength);
 
-  for (let i = LOCATIONS_OFFSET; i < LOCATIONS_OFFSET + LOCATIONS_LENGTH; i += LOCATION_LENGTH){
+  for (let i = LOCATIONS_OFFSET; i < LOCATIONS_OFFSET + LOCATIONS_LENGTH; i += LOCATION_LENGTH) {
     const index: number = i / LOCATION_LENGTH;
-    const entryOffset: number = (view.getUint32(i) >> 8) * ENTRY_LENGTH;
-    const entryLength: number = view.getUint8(i + 3) * ENTRY_LENGTH;
+    const [x, z] = chunkCoordinateFromIndex(index);
+    const byteOffset: number = (view.getUint32(i) >> 8) * ENTRY_LENGTH;
+    const byteLength: number = view.getUint8(i + 3) * ENTRY_LENGTH;
     const timestamp: number = view.getUint32(i + TIMESTAMPS_OFFSET);
-    const entry: Uint8Array | null = entryLength !== 0 ? region.subarray(entryOffset,entryOffset + entryLength) : null;
 
-    entries[index] = { data: entry, index, timestamp, byteOffset: entryOffset, byteLength: entryLength };
-    continue;
-/*
-    // Java code specifically
-
-    if (entry === null){
-      entries[index] = { data: entry, index, timestamp, byteOffset: entryOffset, compression: null };
-      continue;
-    }
-
-    // 0, relative to block
-    const blockByteLength = view.getUint8(entryOffset) - 1;
-    let compression: Compression;
-    const scheme = view.getUint8(entryOffset + 4);
-
-    switch (scheme){
-      case 1: compression = "gzip"; break;
-      case 2: compression = "deflate"; break;
-      case 3: compression = null; break;
-      default: throw new TypeError(`Encountered unsupported compression scheme '${scheme}', must be a valid compression type`);
-    }
-
-    const data = entry.subarray(ENTRY_HEADER_LENGTH,blockByteLength);
-
-    entries[index] = { data, index, timestamp, byteOffset: entryOffset, compression };*/
+    chunks.push({ x, z, index, byteOffset, byteLength, timestamp });
   }
 
-  return entries;
+  return chunks;
 }
 
-export function writeRegion(region: Region): Uint8Array {
-  const byteLength: number = region.reduce((accumulator,entry) => accumulator + (entry.data?.byteLength ?? 0),LOCATIONS_LENGTH + TIMESTAMPS_LENGTH);
-  const data = new Uint8Array(byteLength);
-  const view = new DataView(data.buffer,data.byteOffset,data.byteLength);
+export type Coordinate2D = [number, number];
 
-  const entries: Region = region.toSorted((previous,next) => previous.byteOffset - next.byteOffset);
+export function chunkCoordinateFromIndex(index: number): Coordinate2D {
+  return [
+      index % 32,
+      Math.floor(index / 32) % 32
+  ];
+}
 
-  for (const entry of entries){
-    const headerIndex: number = entry.index * LOCATION_LENGTH;
-    view.setUint32(headerIndex,(entry.byteOffset / ENTRY_LENGTH) << 8);
-    view.setUint8(headerIndex + 3,entry.byteLength / ENTRY_LENGTH);
-    view.setUint32(headerIndex + TIMESTAMPS_OFFSET,entry.timestamp);
-    if (entry.data !== null){
-      data.set(entry.data,entry.byteOffset);
-    }
-  }
-
-  return data;
+export function indexFromChunkCoordinate(coordinate: Coordinate2D): number {
+  const [ x, z ] = coordinate;
+  return (z * 32) + x;
 }
--- test/index.ts
+++ test/index.ts
@@ -1,56 +1,22 @@
 import { readFile } from "node:fs/promises";
-import { ENTRY_HEADER_LENGTH, ENTRY_LENGTH, readRegion, writeRegion } from "../src/index.js";
-
-import type { Region } from "../src/index.js";
+import { chunkCoordinateFromIndex, indexFromChunkCoordinate, readRegion } from "../src/index.js";
 
 const REGION = new URL("./r.2.1.mca",import.meta.url);
 
 const data = await readFile(REGION);
-// console.log(data);
+console.log(data);
 
-// region data
 const region = readRegion(data);
-
-// nice for logging
-const prettyRegion = region;
-// console.log(prettyRegion);
-
-for (const entry of prettyRegion){
-  if (entry.data === null){
-    console.log(null);
-    continue;
-  }
-
-  const start = Buffer.from(entry.data.buffer,entry.data.byteOffset,entry.byteLength);
-  const startLength = start.byteLength;
-
-  const readLength = start.readUInt32BE(0) - 1;
-  const readScheme = start.readUInt8(4);
-  const read = entry.data.subarray(ENTRY_HEADER_LENGTH,ENTRY_HEADER_LENGTH + readLength);
-
-  const writeLength = Math.ceil(readLength / ENTRY_LENGTH) * ENTRY_LENGTH;
-  const write = Buffer.alloc(writeLength);
-  write.writeUInt32BE(readLength + 1,0);
-  write.writeUInt8(readScheme,4);
-  write.set(read,ENTRY_HEADER_LENGTH);
-
-  const compare = Buffer.compare(start,write);
-
-  if (compare === 0) continue;
-  console.log([
-    { startLength },
-    { readLength, readScheme },
-    { writeLength },
-    ["compare:",compare]
-  ]);
-  console.log(start,write);
-}
-
-// const redata = Buffer.from(writeRegion(region).buffer);
-// // console.log(redata);
-
-// console.log("\nequality comparison:",Buffer.compare(data,redata),"== 0");
-
-// function prettyPrint(region: Region): Omit<Region[number], "data">[] {
-//   return region.map(({ index, timestamp, byteOffset }) => ({ index, timestamp, byteOffset }));
-// }
+console.log(
+  // new Set(
+  region
+    .slice(1000)
+    // .map(chunk => {
+    //   const { index } = chunk;
+    //   const coords = chunkCoordinateFromIndex(index);
+    //   const diffIndex = indexFromChunkCoordinate(coords);
+    //   // return index === diffIndex;
+    //   return [index, coords, diffIndex];
+    // })
+  // )
+);
